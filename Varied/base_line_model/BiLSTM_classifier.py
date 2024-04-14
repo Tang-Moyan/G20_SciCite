e668e01824ff1db470301d4eb43da8ae7a666eda
ullbefound <runnaranjarun@gmail.com>
2024-04-14 15:49:29 +0800

    Modification of the BiLSTM and train process

--- Varied/base_line_model/BiLSTM_classifier.py
+++ Varied/base_line_model/BiLSTM_classifier.py
@@ -5,36 +5,33 @@ import torch.nn.init as init
 
 
 class BiLSTMGRUClassifier(nn.Module):
-    def __init__(self, input_dim, embedding_dim, hidden_dim, output_dim, pretrained_embeddings, num_layers=2,
+    def __init__(self, input_dim, embedding_dim, hidden_dim, output_dim, pretrained_embeddings, num_layers=3,
                  dropout=0.2):
         super(BiLSTMGRUClassifier, self).__init__()
         self.embedding = nn.Embedding(input_dim, embedding_dim)
         self.embedding.weight.data.copy_(pretrained_embeddings)
-        self.embedding.weight.requires_grad = False  # 设置为True以在训练中更新权重
+        self.embedding.weight.requires_grad = True  # 设置为True以在训练中更新权重
         self.dropout = nn.Dropout(dropout)
-        self.bilstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, dropout=dropout, bidirectional=True)
-        self.bigru = nn.GRU(hidden_dim * 2, hidden_dim, num_layers=num_layers, dropout=dropout, bidirectional=True)
+        self.bigru = nn.GRU(embedding_dim, hidden_dim, num_layers=num_layers, dropout=dropout, bidirectional=True)
         self.fc = nn.Linear(hidden_dim * 2, output_dim)
         self.hidden_dim = hidden_dim
 
-        # Initialize LSTM and GRU weights orthogonally
-        self._init_orthogonal(self.bilstm)
-        self._init_orthogonal(self.bigru)
+        # Initialize GRU weights with Kaiming initialization
+        self._init_kaiming(self.bigru)
         self.fc.weight.data.normal_(0, 0.01)
         self.fc.bias.data.zero_()
 
     def forward(self, x):
         embedded = self.dropout(self.embedding(x))
-        lstm_output, _ = self.bilstm(embedded)
-        gru_output, _ = self.bigru(lstm_output)
+        gru_output, _ = self.bigru(embedded)
         hidden = torch.cat((gru_output[:, -1, :self.hidden_dim], gru_output[:, 0, self.hidden_dim:]), dim=1)
         return self.fc(hidden)
 
-    def _init_orthogonal(self, layer):
+    def _init_kaiming(self, layer):
         for name, parameter in layer.named_parameters():
             if 'weight_ih' in name:
-                init.orthogonal_(parameter.data)
+                init.kaiming_normal_(parameter.data)
             elif 'weight_hh' in name:
-                init.orthogonal_(parameter.data)
+                init.kaiming_normal_(parameter.data)
             elif 'bias' in name:
                 parameter.data.fill_(0)
--- Varied/train.py
+++ Varied/train.py
@@ -1,9 +1,9 @@
+import time
 import torch
 from torch.utils.data import DataLoader, random_split
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from base_line_model.BiLSTM_classifier import BiLSTMGRUClassifier
-#from myDataset import CustomDataset
 from JsonlDataset import JsonlDataset
 from EDA import EDA
 import json
@@ -18,6 +18,9 @@ MODEL_NAME = "BiLSTMGRUClassifier.pth"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
+# 设置随机种子以确保结果可以复现
+torch.manual_seed(42)
+
 # Initialize EDA and get data
 eda = EDA()
 preprocessed_strings, labels, label_confidence = eda.get_train(save_train=False)
@@ -26,133 +29,90 @@ preprocessed_strings, labels, label_confidence = eda.get_train(save_train=False)
 embedding_model = PretrainedEmbeddingModel(model_name='glove', file_path='glove.42B.300d.npy', test=False)
 embeddings_matrix, _ = embedding_model.get_embeddings_and_vocab_size()
 
-# Create dataset and perform train/validation split
-print("Creating dataset...")
-#dataset = CustomDataset(preprocessed_strings, labels, label_confidence, model_name='glove' embedding_model=embedding_model, use_label_smoothing=False, test=True)
-
-# JsonlDataset is a class that consumes a .jsonl file (like the one given in Scicite) and also an embedding model like the one declared above and returns a PyTorch Dataset object.
-# The dataset contains individual sample of (vector, label, label_confidence) for each item in the JSONL file. The vector is pytorch tensor of the embeddings of the text, label is the label of the text, and label_confidence is the confidence of the label.
+# Create dataset and perform train/validation/test split
 dataset = JsonlDataset(jsonl_file_path='../data/train.jsonl', embedding_model=embedding_model)
+train_size = int(0.7 * len(dataset))
+test_size = int(0.15 * len(dataset))
+val_size = len(dataset) - train_size - test_size
+train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size],
+                                                        generator=torch.Generator().manual_seed(42))
 
-train_size = int(0.8 * len(dataset))
-val_size = len(dataset) - train_size
-print("Splitting dataset into training and validation sets...")
-#train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-
-# Set up DataLoader for both training and validation datasets
-print("Loading data into DataLoader...")
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-print("Finished loading data into DataLoader.")
+# Set up DataLoader for training, validation, and test datasets
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
 # Model setup
-print("Setting up model...")
-embedding_dim = 300
-vocab_size = len(embeddings_matrix)
-model = BiLSTMGRUClassifier(input_dim=vocab_size, embedding_dim=embedding_dim, hidden_dim=256, output_dim=3,
+model = BiLSTMGRUClassifier(input_dim=len(embeddings_matrix), embedding_dim=300, hidden_dim=256, output_dim=3,
                             pretrained_embeddings=embeddings_matrix)
 model.to(device)
 criterion = CrossEntropyLoss()
 optimizer = Adam(model.parameters(), lr=0.001)
 
 # Training and validation loop
-epochs = 20
+epochs = 200
 metrics = {}
-print("Starting training loop...")
 for epoch in tqdm(range(epochs), desc="Training Epochs"):
     model.train()
     total_loss = 0
     num_batches = 0
     for inputs, labels in tqdm(train_loader, leave=False, desc=f"Epoch {epoch + 1} Training"):
-        inputs, labels = inputs.to(device), labels.to(device)  # 将输入数据和标签移动到 GPU 上
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
-
         outputs = model(inputs)
         loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
         total_loss += loss.item()
         num_batches += 1
 
-        loss.backward()
-        optimizer.step()
-
     avg_loss = total_loss / num_batches
-
-    print(f"Epoch {epoch + 1} Loss: {avg_loss}\n")
+    print(f"Epoch {epoch + 1} Loss: {avg_loss}")
 
     if (epoch + 1) % 5 == 0:
-        print("Evaluating on validation set...")
         model.eval()
-        all_preds = []
-        all_labels = []
+        all_preds, all_labels = [], []
         with torch.no_grad():
-            for inputs, labels in tqdm(val_loader, leave=False, desc=f"Epoch {epoch + 1} Validation"):
-                inputs, labels = inputs.to(device), labels.to(device)  # 将输入数据和标签移动到 GPU 上
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
                 outputs = model(inputs)
-                probabilities = torch.softmax(outputs, 1)
-                predicted = torch.argmax(probabilities, 1)
+                predicted = torch.argmax(torch.softmax(outputs, dim=1), dim=1)
                 all_preds.extend(predicted.tolist())
                 all_labels.extend(labels.tolist())
 
-        # Print a label and prediction for debugging
-        print("All Labels:", all_labels)
-        print("All Predictions:", all_preds)
-
-        # Calculate metrics
         accuracy = accuracy_score(all_labels, all_preds)
         precision = precision_score(all_labels, all_preds, average='macro')
         recall = recall_score(all_labels, all_preds, average='macro')
         f1 = f1_score(all_labels, all_preds, average='macro')
+        print(f"Validation Metrics: Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}")
 
-        print(f"Epoch {epoch + 1} Metrics: Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}, Loss: {avg_loss}\n")
+        # Save metrics and model state
+        metrics[epoch + 1] = {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1_score': f1,
+                              'loss': avg_loss}
+        torch.save(model.state_dict(), f"{MODEL_NAME}_epoch_{epoch + 1}.pth")
 
-        metrics[epoch + 1] = {
-            'accuracy': accuracy,
-            'precision': precision,
-            'recall': recall,
-            'f1_score': f1
-        }
-
-        print("Evaluting on training set...")
-        model.eval()
-        all_preds = []
-        all_labels = []
-        with torch.no_grad():
-            for inputs, labels in tqdm(train_loader, leave=False, desc=f"Epoch {epoch + 1} Training"):
-                inputs, labels = inputs.to(device), labels.to(device)  # 将输入数据和标签移动到 GPU 上
-                outputs = model(inputs)
-                _, predicted = torch.max(outputs, 1)
-                all_preds.extend(predicted.tolist())
-                all_labels.extend(labels.tolist())
-
-        # Print a label and prediction for debugging
-        print("All Labels:", all_labels)
-        print("All Predictions:", all_preds)
-
-        # Calculate metrics
-        accuracy = accuracy_score(all_labels, all_preds)
-        precision = precision_score(all_labels, all_preds, average='macro')
-        recall = recall_score(all_labels, all_preds, average='macro')
-        f1 = f1_score(all_labels, all_preds, average='macro')
-
-        print(f"Epoch {epoch + 1} Metrics: Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}, Loss: {avg_loss}\n")
-
-# Save metrics to a JSON file
-with open(f'metrics.json', 'w') as file:
+# Save final metrics to a JSON file
+with open('metrics.json', 'w') as file:
     json.dump(metrics, file, indent=4)
 
-# Save model weights
-torch.save(model.state_dict(), MODEL_NAME)
-print("Metrics saved. Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}")
+print("Training completed.")
 
-#-----------------------------------------------------------
-exit()
-# Do a test load and prediction
-print("Loading model for testing...")
-model = BiLSTMGRUClassifier(input_dim=vocab_size, embedding_dim=embedding_dim, hidden_dim=256, output_dim=3,
-                            pretrained_embeddings=embeddings_matrix)
-model.load_state_dict(torch.load(f'BiLSTMGRUClassifier.pth'))
-model.to(device)
-
-# Predict a test sentence (use model.predict() for a single sentence)
-test_sentence = "The paper is about the COVID-19 pandemic."
+# # Save metrics to a JSON file
+# with open(f'metrics.json', 'w') as file:
+#     json.dump(metrics, file, indent=4)
+#
+# # Save model weights
+# torch.save(model.state_dict(), MODEL_NAME)
+# print("Metrics saved. Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}")
+#
+# # -----------------------------------------------------------
+# exit()
+# # Do a test load and prediction
+# print("Loading model for testing...")
+# model = BiLSTMGRUClassifier(input_dim=vocab_size, embedding_dim=embedding_dim, hidden_dim=256, output_dim=3,
+#                             pretrained_embeddings=embeddings_matrix)
+# model.load_state_dict(torch.load(f'BiLSTMGRUClassifier.pth'))
+# model.to(device)
+#
+# # Predict a test sentence (use model.predict() for a single sentence)
+# test_sentence = "The paper is about the COVID-19 pandemic."
